47e6b07708e77dd1be418342ba10108a0236864b
CODERroot289 <alstine.santosh@gmail.com>
2025-10-12 11:05:17 +0530

    ds

---
+++ src/Menu/Dmenu.jsx
@@ -0,0 +1,71 @@
+// import MyShop from "../pages/dashboard-MyShop.jsx"
+// import "../pages/css/dashboard.css"
+
+// // import { useState,useEffect} from "react";
+// import { Link } from "react-router-dom";
+// import { FaHome, FaUser, FaSignOutAlt, FaBars } from "react-icons/fa";
+// import { useState ,useEffect} from "react";
+// export default function Dmenu() {
+//     const [open, setOpen] = useState(true);
+//     const menuItems = [
+//     { name: "My Account", icon: "fa-solid fa-user", path: "user" },
+//     { name: "Your orders", icon: "fa-solid fa-cart-shopping", path: "cart" },
+//     { name: "sell", icon: "fa-solid fa-store", path: "sell" },
+//     { name: "My Ads ⁽ᶠᵒʳ ᵐᵉᵐᵇᵉʳ⁾", icon: "fa-solid fa-store", path: "shop" },
+//     // { name: "", icon: "fa-solid fa-store", path: "shop" },
+
+//   ];const renderContent = () => {
+    
+
+//     if (activePage === "shop") {
+//        let MyShops =(
+//         <>
+//         <MyShop/>
+//         </>)
+//       return MyShops
+//     }
+    
+
+
+//     if (activePage === "cart"){
+//       let cart =(
+//       <>
+//       <h1>Cart</h1>
+       
+//       </>)
+        
+//       return cart
+    
+
+//     } 
+
+//     if (activePage === "user"){
+//       let User =(
+//       <>
+//         <h1>User</h1>
+//       </>)
+//       return User
+//     } 
+//   };
+
+//      return(
+//       <div className={open ? "sidebar open" : "sidebar"}>
+//         <div className="sidebar-header">
+//           {open && <span className="logo">Menu</span>}
+//           <button className="toggle-btn" onClick={() => setOpen(!open)}>
+//             <FaBars />
+//           </button>
+//         </div>
+//         <ul className="menu">
+//           {menuItems.map((item, index) => (
+//             <li onClick={() => setActivePage(item.path)}  key={index}>
+//               <Link  className="menu-link">
+//                 <span className="icon" ><i className={item.icon}></i></span>
+//                 {open && <span className="text">{item.name}</span>}
+//               </Link>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//       )
+//    }
